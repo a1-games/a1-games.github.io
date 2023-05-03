c10db84c56990e3c6c8e9da638b2e72165a5644b
a1-creator <asbjorn15johnsen@gmail.com>
2023-05-03 21:42:16 +0200

    complete site rework in the making

---
+++ files/js/blogpostloader.js
@@ -0,0 +1,50 @@
+
+var POSTS = [];
+var SORTED_POSTS = [];
+
+var postCount = 0;
+
+var container = document.getElementById("showcase-grid");
+var postTemplate = document.getElementById("post-template");
+
+
+async function getPosts()
+{
+    var list;
+    fetch("https://a1creator.com/files/blogpostlist.json")
+        .then((response) => response.json())
+        .then((jsonresponse) =>  {
+            // [...] makes a copy instead of a reference
+            list = [...jsonresponse.blogposts];
+            postCount = list.length;
+
+            for (let i = 0; i < postCount; i++) {
+                // get rnd index
+                let rnd = Math.floor(Math.random() * list.length);
+                loadAndAddPost(list[rnd]);
+                list.splice(rnd, 1);
+            }
+        });
+    
+}
+
+async function loadAndAddPost(post_id) {
+    // fetch the json game info file
+    fetch(`https://a1games.fun/blogposts/${post_id}/${post_id}.html`)
+        .then((response) => response.text())
+        .then((jsonresponse) =>  {
+
+            // return generated json object
+            POSTS.push(jsonresponse);
+            addItem(jsonresponse);
+        });
+
+}
+
+async function addItem(postObj) 
+{
+    container.append(postObj);
+}
+
+
+getPosts()
