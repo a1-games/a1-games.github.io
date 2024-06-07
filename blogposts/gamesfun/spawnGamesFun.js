c0761e7738d3f4eb64dbb89131c5491862d0cbf2
a1-creator <asbjorn15johnsen@gmail.com>
2024-06-07 15:16:50 +0200

    a1gamesfun added

---
+++ blogposts/gamesfun/spawnGamesFun.js
@@ -0,0 +1,75 @@
+
+// manipulated gamelist with the most PG games
+GAMELIST = [
+    "hack_attack",
+    "crab_island",
+    "cursor_invasion",
+    "pong_around",
+    "fishing_for_humans",
+    //"pissbarrel",
+    "jumpingbacteria",
+    //"dawnville",
+    "crab_defense",
+    "pong",
+    //"pooprun",
+    //"nut_button",
+]
+
+function SpawnRandomizedGames(parent)
+{
+    // [...] makes a copy instead of a reference
+    // GAMELIST is loaded before this script
+    list = [...GAMELIST];
+    gamesCount = list.length;
+
+    for (let i = 0; i < gamesCount; i++) {
+        // get rnd index
+        let rnd = Math.floor(Math.random() * list.length);
+        AddGameImage(list[rnd], parent);
+        list.splice(rnd, 1);
+    }
+    
+}
+
+
+function AddGameImage(gamename, parent)
+{
+    let image = document.createElement("img");
+    image.src = `https://a1games.fun/games/${gamename}/thumbnail.png`;
+    image.classList.add("showcase-img");
+    image.classList.add("gamesfun-game");
+    parent.append(image);
+    return image;
+}
+
+
+
+
+
+// keep it in a function as to not keep variables in memory
+function SpawnGamesFun()
+{
+    // row parent
+    let rowparent = document.createElement("div");
+    rowparent.className = "multi-parent showcase-row-half";
+    rowparent.style.backgroundImage = `url('https://a1games.fun/files/img/mariobck.jpeg')`;
+    rowparent.style.backgroundPosition = `center`;
+
+    // scroll parent
+    let scrollparent = document.createElement("div");
+    scrollparent.style = "cursor: pointer; margin: 0px; position: absolute; top: 50%; transform: translateY(-50%); display: flex; flex-direction: row; align-items: center";
+    scrollparent.onclick = () => {
+        window.open("https://a1games.fun/");
+    };
+
+    let title = SpawnImage(`gamesfun`, `textlogo.png`, scrollparent);
+    title.className = "shape-shadow-black gamesfun-title";
+
+    SpawnRandomizedGames(scrollparent);
+
+    rowparent.append(scrollparent);
+    MainContainer.append(rowparent);
+}
+
+SpawnGamesFun();   
+    
